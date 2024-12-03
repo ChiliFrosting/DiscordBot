6e364c03b7b91ee59bc5363b07f929ff399d0f9e
ChiliFrosting <chillifrosting@gmail.com>
2024-12-04 02:39:46 +0300

    Added broadcaster_login variable for flexibility

--- twitch_user_info.py
+++ twitch_user_info.py
@@ -8,14 +8,15 @@ load_dotenv(override= True)
 
 token= os.getenv("twitch_oauth_token")
 client_id= os.getenv("twitch_client_id")
+broadcaster_login= os.getenv("broadcaster_login")
 
 url= "https://api.twitch.tv/helix/users"
 headers= {"Authorization": f"Bearer {token}",
              "Client-Id": client_id}
-params= {"login": "noxious_tgs"}
+params= {"login": broadcaster_login}
 
 
-get_user_id= requests.get(url= url, headers=headers, params=params)
+get_user_id= requests.get(url= url, headers=headers, params= params)
 data= get_user_id.json()
 #print(data)
 user_id= data["data"][0]["id"]
