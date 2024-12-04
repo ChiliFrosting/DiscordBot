53ae4b1eea811c421844a3d5820d58194af1ac9e
ChiliFrosting <chillifrosting@gmail.com>
2024-12-04 03:03:37 +0300

    Added Async message queue processing to receive messages from websocket client, changed commands directory to -> Commands

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from nextcord.ext import commands
 import os
 from dotenv import load_dotenv
 import asyncio
+from websocket.websocket_message_queue import ws_message_queue
+
 
 #load env variables
 load_dotenv(override= True)
@@ -32,6 +34,21 @@ for filename in os.listdir("./Events"):
         bot.load_extension(f"Events.{filename[:-3]}")
         count+= 1
 print(f"{count} Event Extensions Loaded")
+
+async def process_queue():
+    await bot.wait_until_ready()
+
+    while True: 
+        ws_message= ws_message_queue.get()
+
+        if ws_message["message"] == "subscription_request":
+            channel_id= 1294760925617717373
+            await bot.get_channel(channel_id).send(f"Eventsub subscription request sucessful!\nBroadcaster: {ws_message["content"]["broadcaster"]}\nSubscription Type: {ws_message["content"]["type"]}")
+
+        elif ws_message["message"] == "notification":
+            channel_id= 1294760925617717373
+            await bot.get_channel(channel_id).send(f"{ws_message["content"]["broadcaster"]} is now streaming!")
+
 
 
 #Bot run command
