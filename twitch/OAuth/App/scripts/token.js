6730e1f4b6553e69d229246a4f7138bc70bef6a4
ChiliFrosting <chillifrosting@gmail.com>
2024-12-31 04:37:56 +0300

    js script to extract token from hash and send it to server/web app to update token variable

---
+++ twitch/OAuth/App/scripts/token.js
@@ -0,0 +1,42 @@
+function getToken() {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    return params.get("access_token");
+}
+
+
+async function sendToken(token) {
+    const serverURL = "http://localhost:3000/token";
+
+    try {
+        const response = await fetch (serverURL, {
+            method : "POST", 
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify({token})
+        });
+
+        if (response.ok) {
+            console.log("Token sent");
+        }
+        else {
+            console.log("Failed to send token");
+        }
+    }
+    catch (error) {
+        console.error("Error while sending token: ", error);
+    }
+}
+
+
+window.onload = function() {
+    const token = getToken();
+    const tokenResponse = document.getElementById("token-response");
+
+    if (token) {
+        tokenResponse.textContent = "Token sent!"
+        sendToken(token);
+    }
+    else {
+        tokenResponse.textContent = "Token acquisition failed"
+    }
+}
