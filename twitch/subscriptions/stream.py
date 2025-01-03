9bb89186022fc45fc65fbd403f25d581fc45daa1
ChiliFrosting <chillifrosting@gmail.com>
2025-01-03 16:39:39 +0300

    added stream_info function, finds specified user stream info via HTTP GET - WIP

--- twitch/subscriptions/stream.py
+++ twitch/subscriptions/stream.py
@@ -31,3 +31,22 @@ async def stream_online(session, url, token, client_id, session_id, broadcaster_
         else: 
             status, sub_type, broadcaster= "disabled", None, None
             return status, sub_type, broadcaster
+        
+
+
+
+
+async def stream_info(session, url, token, client_id, user_id, type):
+    headers = {
+        "Authorization" : f"Bearer {token}",
+        "Client-Id" : f"{client_id}",
+    }
+
+    data = {
+        "user_id" : f"{user_id}",
+        "type" : f"{type}"
+    }
+
+    async with session.get(url = url, headers = headers, data = data) as response:
+        response_json = await response.json()
+        print(f"Stream data for user: {user_id}\n\nresponse:\n{response_json}" )
