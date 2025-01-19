7d0678070ca0999405e2bd7489589975a44e6957
ChiliFrosting <chillifrosting@gmail.com>
2025-01-20 02:21:04 +0300

    Idk what I was thinking with whatever was before, still - WIP

--- twitch/async_events.py
+++ twitch/async_events.py
@@ -1,36 +1,19 @@
 
 """
-This module contains all Async events used to start & stop application tasks.
-has class "AsyncEvents" & a single predefined global instance of that class "event".
+This module contains Asyncio event instances for starting/stopping app tasks.
+asyncio.Event() class methods apply.
 
-Other instances can be created as needed which will inherit both events within the class.
+has the following events:
+    - process_queue_event: indicates Bot readiness to receive async queue messages (ready = set, otherwise cleared)
+    - OAuth_valid_event: Indicates whether the OAuth token is valid or invalid/expired (valid = set, otherwise cleared)
 """
 
 import asyncio
 
 
-class AsyncEvents:
-    """
-    Container for Async events related to application tasks.
-    
-    has the following events:
+# Assures Bot not missing any websocket messages
+process_queue_event = asyncio.Event()
 
-        - process_queue_event with asyncio.Event methods
-        - OAuth_is_valid_event with asyncio.Event methods
-    """
-
-    def __init__(self):
-
-        self.process_queue_event = asyncio.Event()
-        """
-        Indicates if the Bot is ready to process messages from Async message queue (websocket_message_queue.ws_message) - (set = ready, clear = not ready).
-        This event assures that no messages are received before the Bot is ready to process them.
-        """
-
-        self.OAuth_is_valid_event = asyncio.Event()
-        """
-        Indicates if the OAuth access token is valid or not - (set = valid, clear = invalid/expired).
-        This event determines whether the websocket client should attempt connection based on the validity of the access token.
-        Also determines of the web server should be run allowing for the generation of new access tokens.
-        """
-
+# Determines whether the websocket client should attemp reconnection
+# Determines whether the web server is run to generate a new token
+OAuth_valid_event = asyncio.Event()
