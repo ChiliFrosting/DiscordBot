3474f623f358e43650513442c07d06adb731f499
ChiliFrosting <chillifrosting@gmail.com>
2025-02-11 17:49:03 +0300

    Added scoring function to flag possible spam/scam/bot accounts

--- bot/Events/member_events.py
+++ bot/Events/member_events.py
@@ -24,6 +24,97 @@ class member_Events(commands.Cog):
         self.bot = bot
         self.unverified = {}
 
+
+    async def verification_check(self, member: nextcord.Member) -> None:
+        """
+        Helper function to check if the member completed verification
+
+        Args:
+            member (nextcord.Member): passed as the member that triggered the event
+        """
+
+        await asyncio.sleep(30)
+        role = nextcord.utils.get(member.guild.roles, name = verified_role_name)
+
+        if member.id in self.unverified:
+            del self.unverified[member.id]
+            if role not in member.roles: 
+                await member.kick(reason = f"{member.name} did not verify within grace period")
+                await self.bot.get_channel(mod_channel).send(f"Member {member.name} was kicked, did not attempt verification.")
+
+
+    async def threat_score(self, member: nextcord.Member) -> tuple[str, bool, bool]:
+        """
+        This function assigns a score to the member which determines their ability to initiate 
+        the verification flow automatically, require manual review or be kicked immediately
+
+        Score is determined as follows: 
+            - Account age (< 7 days) -> 40 points
+            - Bot (Discord app) account -> auto-restrict
+            - Default avatar -> 20
+            - known spammer -> auto-restrict
+            - Custom avatar -> -5 points
+            - Custom banner -> -5 points
+            - HypeSquad membership -> -10 points
+            - Verified account -> -10 points
+
+
+        If member has a score of less than 20 -> auto verify available
+        If member has a score of 20 or more -> manual review
+
+
+        ## Args:
+            - member (nextcord.Member): passed as member that triggered the event
+
+
+        ## Returns:
+            tuple: a tuple with: 
+                - str: suggested action 
+                - bool: if member is a bot, True = Yes
+                - bool: if member is flagged as known spammer by discord, True = Yes
+        """
+
+        score = 0
+        is_bot = False
+        is_spam = False
+
+        account_age = (nextcord.utils.utcnow() - member.created_at).days
+        hypesquad_member = nextcord.PublicUserFlags.hypesquad
+        spammer = nextcord.PublicUserFlags.known_spammer
+
+        if account_age <= 7:
+            score += 40
+
+        if member.bot:
+            is_bot = True
+        
+        if member.default_avatar:
+            score += 20
+
+        elif member.avatar:
+            score -= 5
+
+        if member.banner:
+            score -= 5
+
+        if member.pending is False:
+            score -= 10
+
+        if hypesquad_member in member.public_flags:
+            score -= 10
+
+        if spammer in member.public_flags:
+            is_spam = True
+
+        if score >= 20:
+            action = "manual_review"
+            return action, is_bot, is_spam
+        
+        elif score < 20: 
+            action = "auto_review"
+            return action, is_bot, is_spam
+        
+
     @commands.Cog.listener()
     async def on_member_join(self, member: nextcord.Member) -> None:
         """
@@ -34,8 +125,8 @@ class member_Events(commands.Cog):
 
         This listener requires the member intents [Intents.members] to function properly
 
-        Args:
-            member (nextcord.Member): The member that triggered the event (member joining the server/guild in this case)
+        ## Args:
+            - member (nextcord.Member): Member that triggered the event (member joining the server/guild in this case)
         """
 
         await self.bot.get_channel(mod_channel).send(f"User: {member.name} has joined the server on {date.today()}")
@@ -52,19 +143,6 @@ class member_Events(commands.Cog):
         print (self.unverified)
 
         await self.verification_check(member)
-
-
-    async def verification_check(self, member: nextcord.Member) -> None:
-        await asyncio.sleep(30)
-        role = nextcord.utils.get(member.guild.roles, name = verified_role_name)
-
-        if member.id in self.unverified:
-            del self.unverified[member.id]
-            if role not in member.roles: 
-                await member.kick(reason = f"{member.name} did not verify within grace period")
-                await self.bot.get_channel(mod_channel).send(f"Member {member.name} was kicked, did not attempt verification.") 
-            
-
 
 
 # Register the class/cog with the bot, supports loading/unloading although not currently implemented           
